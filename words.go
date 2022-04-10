faa9f6a6094f953f15341e9f090c682c49ca6b02
garlic0x1 <43155857+garlic0x1@users.noreply.github.com>
2022-04-10 12:56:44 -0700

    Add files via upload

--- words.go
+++ words.go
@@ -6,6 +6,7 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"net/url"
 	"os"
@@ -27,10 +28,24 @@ func isUnique(url string) bool {
 }
 
 func main() {
+	// options
+	keys := flag.Bool("keys", false, "Use parameter keys.")
+	vals := flag.Bool("vals", false, "Use parameter values.")
+	paths := flag.Bool("paths", false, "Use URL paths.")
+	domains := flag.Bool("domains", false, "Use domain names.")
+	flag.Parse()
+
+	if !(*keys) && !(*vals) && !(*paths) && !(*domains) {
+		*keys = true
+		*vals = true
+		*paths = true
+		*domains = true
+	}
+
 	// check for stdin
 	stat, _ := os.Stdin.Stat()
 	if (stat.Mode() & os.ModeCharDevice) != 0 {
-		fmt.Fprintln(os.Stderr, "No input detected, use `cat scope | gau | words`")
+		fmt.Fprintln(os.Stderr, "No input detected, use `cat urls.txt | words`")
 		os.Exit(1)
 	}
 
@@ -48,19 +63,27 @@ func main() {
 				path := parsed.Path
 				query := parsed.Query()
 
-				for _, w := range strings.Split(host, ".") {
-					results <- w
+				if *domains {
+					for _, w := range strings.Split(host, ".") {
+						results <- w
+					}
 				}
-				for _, w := range strings.Split(path, "/") {
-					results <- w
+				if *paths {
+					for _, w := range strings.Split(path, "/") {
+						results <- w
+					}
 				}
 				for k, v := range query {
-					results <- k
-					for _, v1 := range v {
-						for _, v2 := range strings.Split(v1, "/") {
-							results <- v2
+					if *keys {
+						results <- k
+					}
+					if *vals {
+						for _, v1 := range v {
+							for _, v2 := range strings.Split(v1, "/") {
+								results <- v2
+							}
+							results <- v1
 						}
-						results <- v1
 					}
 				}
 			}
